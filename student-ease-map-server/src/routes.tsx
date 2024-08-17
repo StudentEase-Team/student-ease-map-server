1f9799d798b4a8b88649f3aa6fb60f36695d37df
Ognjen Milojevic <ognjenmilojevic2001@gmail.com>
2024-08-17 17:42:40 +0200

    feat: almost finished main building

--- student-ease-map-server/src/routes.tsx
+++ student-ease-map-server/src/routes.tsx
@@ -6,6 +6,9 @@ import NastavniBlok1Prikaz from './pages/nastavni-blok-1';
 import NastavniBlok2Prikaz from './pages/nastavni-blok-2';
 import NastavniBlokUcionice1Prikaz from './pages/nastavni-blok-ucionice-1';
 import NastavniBlokUcionice2Prikaz from './pages/nastavni-blok-ucionice-2';
+import NastavniBlokUcionice3Prikaz from './pages/nastavni-blok-ucionice-3';
+import NastavniBlokUcionice4Prikaz from './pages/nastavni-blok-ucionice-4';
+import NastavniBlokUcionice5Prikaz from './pages/nastavni-blok-ucionice-5';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -16,6 +19,9 @@ const AppRoutes: React.FC = () => {
       <Route path="/nastavniblok/2" element={<NastavniBlok2Prikaz />} />
       <Route path="/nastavniblok/ucionice/1" element={<NastavniBlokUcionice1Prikaz />} />
       <Route path="/nastavniblok/ucionice/2" element={<NastavniBlokUcionice2Prikaz />} />
+      <Route path="/nastavniblok/ucionice/3" element={<NastavniBlokUcionice3Prikaz />} />
+      <Route path="/nastavniblok/ucionice/4" element={<NastavniBlokUcionice4Prikaz />} />
+      <Route path="/nastavniblok/ucionice/5" element={<NastavniBlokUcionice5Prikaz />} />
     </Routes>
   );
 };
